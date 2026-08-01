cc2011742a5b910a33c5093f9fe65124d708b245
LonelyDawnStar <smg090527@gmail.com>
2026-08-02 07:43:00 +0900

    Fix Gradle 9 processResources configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.language.jvm.tasks.ProcessResources
+
 plugins {
     java
 }
@@ -22,8 +25,8 @@ tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
 }
 
-processResources {
+tasks.named<ProcessResources>("processResources") {
     filesMatching("plugin.yml") {
-        expand("version" to project.version)
+        expand("version" to project.version.toString())
     }
 }
